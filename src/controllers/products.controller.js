b6e989de5aed90ee2ebe36f8c830fa79c01ca1d2
Nico San Pedro <nicolassanpedro@gmail.com>
2021-02-28 21:13:51 -0300

    added update and delete product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,35 +1,36 @@
-import Product from '../models/Product';
+import Product from "../models/Product";
 
+export const createProduct = async (req, res) => {
+  const { name, category, price, imgURL } = req.body;
 
-export const createProduct  = async (req, res) => {
+  const productToCreate = new Product({ name, category, price, imgURL });
 
-    const { name, category, price, imgURL } = req.body;
+  const productSave = await productToCreate.save();
 
-    const productToCreate = new Product({name, category, price, imgURL});
-
-    const productSave = await productToCreate.save();
-
-    res.status(201).json(productSave);
-}
+  res.status(201).json(productSave);
+};
 
 export const getProducts = async (req, res) => {
-   const products = await Product.find();
-   res.status(200).json(products); 
-}
-
+  const products = await Product.find();
+  res.status(200).json(products);
+};
 
 export const getProductById = async (req, res) => {
-    const { productID } = req.params;
-    const productByID = await Product.findById(productID)
-    res.status(200).json(productByID);
-}
-
+  const { productID } = req.params;
+  const productByID = await Product.findById(productID);
+  res.status(200).json(productByID);
+};
 
 export const updateProductById = async (req, res) => {
-    res.json('UPDATE');
-}
-
+  const { productID } = req.params;
+  const updatedProduct = await Product.findByIdAndUpdate(productID, req.body, {
+      new: true
+  });
+  res.status(204).json(updatedProduct);
+};
 
 export const deleteProductById = async (req, res) => {
-    res.json('DELETE');
-}
+  const { productID } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(productID);
+  res.status(200).json(deleteProduct);
+};
