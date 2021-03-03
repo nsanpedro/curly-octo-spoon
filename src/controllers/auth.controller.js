f636b8ac33f1875c3ee2a6e85f3b14e3309612b3
Nico San Pedro <nicolassanpedro@gmail.com>
2021-03-02 21:28:44 -0300

    final touches sign in

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
   const { userName, email, password, roles } = req.body;
@@ -11,7 +12,16 @@ export const signUp = async (req, res) => {
     password: await User.encryptPassword(password),
   });
 
+  if (roles) {
+    const rolesFound = await Role.find({ name: { $in: roles } });
+    createdUser.roles = rolesFound.map((role) => role._id);
+  } else {
+    const role = await Role.findOne({ name: "User" });
+    createdUser.roles = [role._id];
+  }
+
   const savedUser = await createdUser.save();
+  console.log("save user ==> ", savedUser);
 
   const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
     expiresIn: 86400,
